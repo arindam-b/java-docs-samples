19a2f0d79ed45d97ec2e44e0ec2363e6c470e3c7
arindam-b <banerjee1.arindam@gmail.com>
2023-07-13 16:20:11 +0200

    PaLM api client - java - Junit test cases

--- palm-api-client-java17/spring/src/test/java/com/example/palmapi/PalmApiJavaApplicationTests.java
+++ palm-api-client-java17/spring/src/test/java/com/example/palmapi/PalmApiJavaApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.palmapi;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PalmApiJavaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+    private PromptApi promptApi;
+
+    @Test
+    public void testGetPrediction() throws IOException {
+        String question = "What is the meaning of life?";
+        String content = (String) promptApi.getPrediction(question);
+        System.out.println(content);
+    }
 
 }
